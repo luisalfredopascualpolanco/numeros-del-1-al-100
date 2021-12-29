ab7e545c78e69dda46ddd1c9e27034382058b7d9
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-28 16:31:06 -0800

    version1.0

---
+++ proyecto10.1/proyecto10.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numero = 0;
+
+                for (int i = 0; i < 100; i++)
+                {
+                    numero++;
+                    if (numero<100)
+                    {
+                        Console.Write(" " + numero + " -");
+                    }
+                    else
+                    {
+                        Console.Write(" "+numero);
+                    }
+                    
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un valor invalido");
+            }
+            Console.ReadKey();
+        }
+    }
+}
